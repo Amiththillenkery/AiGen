8566f62934abc89b392a5501af5e672bdf0d4143
Amiththillenkery <anithamith@gmail.com>
2026-03-09 02:42:50 +0530

    feat(create-a-dotnet-repository-with-clean-architecture): implement Create a dotnet repository with clean architecture and minimal API

---
+++ src/Aigen.Application/DTOs/BaseDto.cs
@@ -0,0 +1,8 @@
+namespace CreateADotnetRepositoryWithCleanArchitecture.Application.DTOs
+{
+    public record BaseDto
+    {
+        public Guid Id { get; init; }
+        public DateTime CreatedAt { get; init; }
+    }
+}
