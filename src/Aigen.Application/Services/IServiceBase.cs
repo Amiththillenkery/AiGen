9bd456680cbefee1fd444acd6e79181c53847412
Amiththillenkery <anithamith@gmail.com>
2026-03-09 16:42:57 +0530

    feat(create-user-enity-in-the-existing-db-context): implement Create User enity in the existing DB context

--- src/Aigen.Application/Services/IServiceBase.cs
+++ src/Aigen.Application/Services/IServiceBase.cs
@@ -1,13 +1,13 @@
-namespace CreateADotnetRepositoryWithCleanArchitecture.Application.Services
-{
-    using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
+namespace CreateUserEnityInTheExistingDbContext.Application.Services
+{
     public interface IServiceBase<TDto, TCreateDto, TUpdateDto>
     {
-        Task<IEnumerable<TDto>> GetAllAsync(CancellationToken cancellationToken);
         Task<TDto> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<TDto>> GetAllAsync(CancellationToken cancellationToken);
         Task<TDto> CreateAsync(TCreateDto createDto, CancellationToken cancellationToken);
         Task<TDto> UpdateAsync(int id, TUpdateDto updateDto, CancellationToken cancellationToken);
         Task<bool> DeleteAsync(int id, CancellationToken cancellationToken);
